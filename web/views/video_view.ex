fac88ea53a3bf0d427f5707bf68bd5d981cc24d9
Jim Menard <jim@jimmenard.com>
2016-01-01 10:21:41 -0500

    generic name is better

--- web/views/video_view.ex
+++ web/views/video_view.ex
@@ -1,7 +1,7 @@
 defmodule Rumbl.VideoView do
   use Rumbl.Web, :view
 
-  def truncated_url(url, maxlen \\ 30) do
+  def truncate(url, maxlen \\ 30) do
     if String.length(url) > maxlen do
       String.slice(url, 0, maxlen) <> "..."
     else
