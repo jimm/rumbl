79bc8a4874180e151df51675aa636cc0898ac8c3
Jim Menard <jim.menard@chloeandisabel.com>
2016-01-11 07:29:10 -0500

    Fixed seeds.

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -3,7 +3,5 @@ alias Rumbl.Category
 
 for category <- ~w(Action Drama Romance Comedy Sci-Fi Music) do
   cs = %Category{} |> Category.changeset(%{name: category})
-  unless cs.errors do
-    Repo.insert! cs
-  end
+  Repo.insert! cs
 end
